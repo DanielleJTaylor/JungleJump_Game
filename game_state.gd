7e6d8057da752bd5873b9c0bde59c35236f49777
DanielleJTaylor <179940625+DanielleJTaylor@users.noreply.github.com>
2025-02-07 13:54:52 -0500

    Add files via upload

---
+++ game_state.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var num_levels: int = 3
+
+var current_level: int = 0
+var game_scene: String = "res://main/main.tscn"
+var title_screen: String = "res://ui/title_scene.tscn"
+
+func restart() -> void:
+	current_level = 0
+	get_tree().change_scene_to_file(title_screen)
+
+func next_level() -> void:
+	current_level += 1
+	if current_level <= num_levels:
+		call_deferred("_change_to_scene", game_scene)
+	else:
+		restart()
+		
+func _change_to_scene(scene_path: String) -> void:
+	get_tree().change_scene_to_file(scene_path)
